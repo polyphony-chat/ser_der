d89ed1b8aab190bebe2d2c5b7b8a2471c2b2efa3
bitfl0wer <florian@pro-weber.com>
2024-05-05 13:12:55 +0200

    license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+// Copyright (c) 2024 bitfl0wer
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
